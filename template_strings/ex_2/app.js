808b09f8b66a5856c879522dcdd0944da16cb4cf
Matteo <Matteoshurdhaqi@gmail.com>
2023-11-21 13:08:08 +0100

    esercizi template completati

---
+++ template_strings/ex_2/app.js
@@ -0,0 +1,4 @@
+const string = `lorem ipsum dolor sit amet, 
+consectetur adipiscing et, 
+sed diam nonumy eirmod tempor incididunt ut`;
+console.log(string);
---
+++ template_strings/ex_3/app.js
@@ -0,0 +1,6 @@
+//Crea una template string che genera un messaggio di saluto con un nome e un messaggio personalizzato.
+
+const name = "John";
+const message = "Welcome to our website!";
+const greeting = `Hi ${name}, ${message}`;
+console.log(greeting);
